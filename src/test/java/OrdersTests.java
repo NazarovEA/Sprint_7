e2e4814f8bc235d0fd7cd2421c472b697baa4753
Евгений <nk-nazarovEA@yandex.ru>
2026-02-18 00:06:06 +0300

    Тесты создания заказа:успешное создание заказа 201

---
+++ src/test/java/OrdersTests.java
@@ -0,0 +1,35 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class OrdersTests {
+    // аннотация BeforeEach показывает, что метод будет выполняться перед каждым тестовым методом
+    @BeforeEach
+    public void setUp() {
+        // повторяющуюся для разных ручек часть URL лучше записать в переменную в методе Before
+        // если в классе будет несколько тестов, указывать её придётся только один раз
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru/";
+    }
+
+    @Test //успешное создание заказа 201
+    public void successfulOrderCreate() {
+        String json = "{\"firstName\": \"Naruto\",\"lastName\": \"Uchiha\",\"address\": \"Konoha, 142 apt.\",\"metroStation\": 4,\"phone\": \"+7 800 355 35 35\",\"rentTime\": 5,\"deliveryDate\": \"2020-06-06\",\"comment\": \"Saske, come back to Konoha\",\"color\": [\"BLACK\"]}";
+        Response response =
+                // метод given() помогает сформировать запрос
+                given()
+                        .header("Content-type", "application/json")
+                        // указываем протокол и данные авторизации
+                        .auth().oauth2("lih2TK06FLsu5HQ32PR6XzCsmg0GVPrL2seXkQVprx5FhRzNK8ArtT7u42RhqegQ")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/orders");
+        response.then().assertThat().body("track", notNullValue()) //как вернуть ответ с айдишником
+                .and()
+                .statusCode(201);
+    }
+}

56488f35bd9c2dea3dc176b492403185556e3334
Евгений <nk-nazarovEA@yandex.ru>
2026-02-17 23:49:50 +0300

    Тесты логина:запрос с несуществующими логином и паролем 404

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -49,4 +49,21 @@ public class LoginTests {
                 .and()
                 .statusCode(400);
     }
+    @Test //запрос с несуществующими длгином и паролем 404
+    public void doesNotExistLoginAndPass() {
+        String json = "{\"login\": \"buba\",\"password\": \"1488\"}";
+        Response response =
+                // метод given() помогает сформировать запрос
+                given()
+                        .header("Content-type", "application/json")
+                        // указываем протокол и данные авторизации
+                        .auth().oauth2("lih2TK06FLsu5HQ32PR6XzCsmg0GVPrL2seXkQVprx5FhRzNK8ArtT7u42RhqegQ")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier/login");
+        response.then().assertThat().body("message", notNullValue()) //как вернуть ответ с айдишником
+                .and()
+                .statusCode(404);
+    }
 }

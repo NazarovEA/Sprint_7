bdfda14c6ca18eb922a49bd8ca5af08b091ffbb4
Евгений <nk-nazarovEA@yandex.ru>
2026-02-18 23:22:50 +0300

    Сделал на тесте создание курьера poj модель, степ применил на создании курьера, и генерацию логина...

--- src/test/java/CourierTests.java
+++ src/test/java/CourierTests.java
@@ -1,13 +1,18 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import models.CreateCourierModel;
 import org.hamcrest.Matcher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class CourierTests {
+    Random random = new Random();
     // аннотация BeforeEach показывает, что метод будет выполняться перед каждым тестовым методом
     @BeforeEach
     public void setUp() {
@@ -18,28 +23,39 @@ public class CourierTests {
 
     @Test //курьера можно создать
     public void createCourier() {
-        String json = "{\"login\": \"nini454\",\"password\": \"12345\",\"firstName\": \"ninini\"}";
+        CreateCourierModel createCourierModel = new CreateCourierModel();
+        // Генерируем уникальный логин, чтобы не было конфликтов
+        createCourierModel.setLogin("Login_" + System.currentTimeMillis());
+        createCourierModel.setPassword("P@ss123");
+        createCourierModel.setFirstName("Ivan");
+        //String json = "{\"login\": \"nini48954\",\"password\": \"12345\",\"firstName\": \"ninini\"}";
                 // метод given() помогает сформировать запрос
         Response response =
         given()
+                //.log().all() // Лог запроса
                 .header("Content-type", "application/json")
                 // указываем протокол и данные авторизации
                 .auth().oauth2("lih2TK06FLsu5HQ32PR6XzCsmg0GVPrL2seXkQVprx5FhRzNK8ArtT7u42RhqegQ")
                 .and()
-                .body(json)
+                .body(createCourierModel)
                 .when()
                 .post("/api/v1/courier");
-        response.then().assertThat().body("ok", notNullValue())
+        response.then()
+                //.log().all() // Лог запроса
+                .assertThat().body("ok", notNullValue())
                 .and()
                 .statusCode(201);
         System.out.println(response.body().asString());
     }
+
     @Test //нельзя создать двух одинаковых курьеров c одним логином
     public void createCourierOld() {
-        String json = "{\"login\": \"nini8\",\"password\": \"12345\",\"firstName\": \"ninini\"}";
+        String name = "Login_" + System.currentTimeMillis();
+        String json = "{\"login\": \"" + name + "\",\"password\": \"12345\",\"firstName\": \"ninini\"}";
         Response response =
         // метод given() помогает сформировать запрос
         given()
+                //.log().all()
                 .header("Content-type", "application/json")
                 // указываем протокол и данные авторизации
                 .auth().oauth2("lih2TK06FLsu5HQ32PR6XzCsmg0GVPrL2seXkQVprx5FhRzNK8ArtT7u42RhqegQ")
@@ -47,11 +63,30 @@ public class CourierTests {
                 .body(json)
                 .when()
                 .post("/api/v1/courier");
-        response.then().assertThat().body("message", notNullValue())
+        response.then()
+                //.log().all()
+                .assertThat().body("ok", notNullValue())
+                .and()
+                .statusCode(201);
+        Response response2 =
+        given()
+                //.log().all()
+                .header("Content-type", "application/json")
+                // указываем протокол и данные авторизации
+                .auth().oauth2("lih2TK06FLsu5HQ32PR6XzCsmg0GVPrL2seXkQVprx5FhRzNK8ArtT7u42RhqegQ")
+                .and()
+                .body(json)
+                .when()
+                .post("/api/v1/courier");
+        response2.then()
+                //.log().all()
+                .assertThat().body("message", equalTo("Этот логин уже используется. Попробуйте другой."))
                 .and()
                 .statusCode(409);
-        System.out.println(response.body().asString());
+        System.out.println(response2.body().asString());
     }
+
+
     @Test //чтобы создать курьера, нужно передать в ручку все обязательные поля.Передаем без логина и пароляц
     public void createCourierNotLoginAndPassword() {
         String json = "{\"firstName\": \"ninini\"}";
@@ -68,6 +103,25 @@ public class CourierTests {
         response.then().assertThat().body("message", notNullValue())
                 .and()
                 .statusCode(400);
+        System.out.println(response.body().asString());
+    }
+
+    @Test //курьера можно создать
+    public void createCourier2() {
+        CreateCourierModel createCourierModel = new CreateCourierModel();
+        // Генерируем уникальный логин, чтобы не было конфликтов
+        createCourierModel.setLogin("Login_" + System.currentTimeMillis());
+        createCourierModel.setPassword("P@ss123");
+        createCourierModel.setFirstName("Ivan");
+        //String json = "{\"login\": \"nini48954\",\"password\": \"12345\",\"firstName\": \"ninini\"}";
+        // метод given() помогает сформировать запрос
+        // Вызываем степ и сохраняем результат в переменную response
+        Response response = CourierSteps.createCourierStep(createCourierModel);
+        response.then()
+                //.log().all() // Лог запроса
+                .assertThat().body("ok", notNullValue())
+                .and()
+                .statusCode(201);
         System.out.println(response.body().asString());
     }
 }
